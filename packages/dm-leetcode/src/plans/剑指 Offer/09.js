bc9d921c8245ab7d11253fe08559921c80aa48ca
徐泽雯 <xuzewen@bilibili.com>
2023-02-06 19:13:40 +0800

    feat: Cqueue

--- packages/dm-leetcode/src/plans/剑指 Offer/09.js
+++ packages/dm-leetcode/src/plans/剑指 Offer/09.js
@@ -1,5 +1,12 @@
-var CQueue = function() {
-
+/**
+ * 用两个栈实现队列
+ * 栈：先进后出，用数组只能用 push 和 pop 方法
+ * 像玩汉诺塔
+ * @url https://leetcode.cn/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+ */
+const CQueue = function() {
+  this.inStack = []
+  this.outStack = []
 };
 
 /**
@@ -8,14 +15,21 @@ var CQueue = function() {
  * @url https://leetcode.cn/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
  */
 CQueue.prototype.appendTail = function(value) {
-
+  this.inStack.push(value)
 };
 
 /**
  * @return {number}
  */
 CQueue.prototype.deleteHead = function() {
-
+  // 当 outStack 为空，说明可以输出正确顺序的栈空了。需要从 inStack 里再进点货
+  if (this.outStack.length === 0) {
+    if (this.inStack.length === 0) return -1
+    while(this.inStack.length !== 0) {
+      this.outStack.push(this.inStack.pop())
+    }
+  }
+  return this.outStack.pop()
 };
 
 /**
@@ -25,10 +39,10 @@ CQueue.prototype.deleteHead = function() {
  * var param_2 = obj.deleteHead()
  */
 const obj = new CQueue();
-obj.appendTail(3)
-obj.deleteHead()
-obj.deleteHead()
-obj.deleteHead()
+console.log(obj.appendTail(3))
+console.log(obj.deleteHead())
+console.log(obj.deleteHead())
+console.log(obj.deleteHead())
 
 // const obj = new CQueue();
 // obj.deleteHead()
