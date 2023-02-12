b093a090d98e7496a03c656866ddf739a633beb2
俞思真 <carl@sizhendeMacBook-Pro.local>
2023-02-12 20:48:21 +0800

    ci: init java env
    1. init java env with gradle
    2. init integration-tests project it-test
    3. init totally version control management project platform

---
+++ dm-java/it/src/main/java/com/zoe/it/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.zoe.it;
+
+/**
+ * @author carl
+ * @since 2023-02-12 8:47 PM
+ **/
+public class HelloWorld {
+
+
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
+
+}
